c40f21cc51f45ebc39720196d60989e5669b2399
Aaron Greenspan <afg419@gmail.com>
2015-10-23 10:48:44 -0600

    Got a tree. can add values. can detect values.

---
+++ recursive_binary_tree.rb
@@ -0,0 +1,66 @@
+
+require 'pry'
+
+class BinaryTree
+
+  attr_accessor :root, :left, :right, :bool
+
+  def initialize(data)
+    @root = data
+    @left = nil
+    @right = nil
+  end
+
+  def add_node(data)
+
+    # H = { true => { true => make new node set to left  ,
+    #                false => left add_node },
+    #      false => { true => make new node set to right ,
+    #                false => right add_node }}
+    #
+    #   H[data < root][data < root ? left.nil? : right.nil?]
+
+    if left.nil? && data < root
+      @left = BinaryTree.new(data)
+    elsif right.nil? && data > root
+      @right = BinaryTree.new(data)
+    elsif data < root
+      left.add_node(data)
+    elsif data > root
+      right.add_node(data)
+    end
+
+  end
+
+
+  def include?(data)
+    bool = false
+    if data == root
+      bool = true
+    elsif left.nil? && data < root
+      bool = false
+    elsif right.nil? && data > root
+      bool = false
+    elsif data < root
+      bool = left.include?(data)
+    elsif data > root
+      bool = right.include?(data)
+    end
+    bool
+  end
+
+
+
+end
+
+tree = BinaryTree.new(10)
+puts tree.left
+puts tree.right
+tree.add_node(11)
+
+puts tree.right.root
+tree.add_node(12)
+tree.add_node(10.5)
+puts tree.right.right.root
+puts tree.right.left.root
+puts tree.include?(10.5)
---
+++ recursive_binary_tree_test.rb
@@ -0,0 +1,96 @@
+
+require 'minitest'
+require 'pry'
+require_relative 'recursive_binary_tree'
+
+class BinaryTreeTest < Minitest::Test
+
+  attr_reader :test_tree, :inputs
+
+  def setup
+    @test_tree = BinaryTree.new(0)
+    @inputs = (0..1000).to_a.shuffle
+  end
+
+  def test_class_exists
+    assert BinaryTree
+  end
+
+  def test_can_initialize_with_data_in_root
+    tree = BinaryTree.new(10)
+    assert_equal 10, tree.root
+  end
+
+  def test_initializes_with_no_other_trees
+    tree = BinaryTree.new(15)
+    assert tree.left.nil?
+    assert tree.right.nil?
+  end
+
+  def test_adds_a_tree_to_right
+    tree = BinaryTree.new(10)
+    tree.add_node(11)
+    assert_equal 11 , tree.right.root
+  end
+
+  def test_adds_a_tree_to_left
+    tree = BinaryTree.new(10)
+    tree.add_node(9)
+    assert_equal 9 , tree.left.root
+  end
+
+  def test_adds_a_tree_right_and_left
+    tree = BinaryTree.new(10)
+    tree.add_node(9)
+    tree.add_node(11)
+    assert_equal 9, tree.left.root
+    assert_equal 11, tree.right.root
+  end
+
+  def test_adds_a_tree_right_right
+    tree = BinaryTree.new(10)
+    tree.add_node(11)
+    tree.add_node(12)
+    assert_equal 11, tree.right.root
+    assert_equal 12, tree.right.right.root
+  end
+
+
+  def test_adds_a_tree_right_left
+    tree = BinaryTree.new(10)
+    tree.add_node(12)
+    tree.add_node(11)
+    assert_equal 12, tree.right.root
+    assert_equal 11, tree.right.left.root
+  end
+
+  def test_can_detect_value_in_tree
+    tree = BinaryTree.new(10)
+    inputs = [11,9,4,12,5,14,13]
+    inputs.each do |input|
+      tree.add_node(input)
+    end
+
+    assert tree.include?(14)
+    refute tree.include?(1)
+
+  end
+
+  def test_can_detect_random_values_in_tree
+    tree = BinaryTree.new(500)
+    library = (0..1000).to_a.shuffle
+    library.delete(500)
+    inputs = library[0..500]
+    test_val = library.sample
+
+    inputs.each do |input|
+      tree.add_node(input)
+    end
+    puts "#{inputs.include?(test_val)}"
+    assert_equal inputs.include?(test_val) , tree.include?(test_val)
+  end
+
+
+
+
+end
